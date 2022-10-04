23f4fb44d50bf142c88faa10b8c107751caa4a57
Trond Jakob Sjøvang <trond.sjovang@atea.no>
2022-10-04 10:26:59 +0000

    security rules

--- modules/active-directory/main.tf
+++ modules/active-directory/main.tf
@@ -15,6 +15,51 @@ resource "azurerm_network_security_group" "ad_ds" {
   resource_group_name = azurerm_resource_group.active_directory.name
 
   security_rule {
+    name              = "in_allow_ad_tcp"
+    priority          = 100
+    direction         = "Inbound"
+    access            = "Allow"
+    protocol          = "Tcp"
+    source_port_range = "*"
+    destination_port_ranges = [
+      "53",
+      "88",
+      "135",
+      "139",
+      "389",
+      "445",
+      "464",
+      "636",
+      "3268-3269",
+      "40152-65535",
+    ]
+    source_address_prefix = "VirtualNetwork"
+    destination_application_security_group_ids = [
+      azurerm_application_security_group.ad_ds.id
+    ]
+  }
+
+  security_rule {
+    name              = "in_allow_ad_udp"
+    priority          = 110
+    direction         = "Inbound"
+    access            = "Allow"
+    protocol          = "Udp"
+    source_port_range = "*"
+    destination_port_ranges = [
+      "53",
+      "88",
+      "123",
+      "389",
+      "464",
+    ]
+    source_address_prefix = "VirtualNetwork"
+    destination_application_security_group_ids = [
+      azurerm_application_security_group.ad_ds.id
+    ]
+  }
+
+  security_rule {
     name                       = "in_deny_all"
     priority                   = 4096
     direction                  = "Inbound"
